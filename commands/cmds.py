1a78d3a3100df6c4d0a53d7296f948f9b11e91c8
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-03 10:13:37 +0100

    Update cmds.py

--- commands/cmds.py
+++ commands/cmds.py
@@ -1,6 +1,8 @@
 from commands.slash.help import Help
 from commands.slash.wse import Wse
+from commands.slash.bancheck.lastbans import LastBans
 
 def setup(bot):
     bot.add_cog(Help(bot))
     bot.add_cog(Wse(bot))
+    bot.add_cog(LastBans(bot))
