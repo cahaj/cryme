ce1a09aaa3a6a00dd743068d9fd904d2efe7758d
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-07 19:11:21 +0100

    Create antisniper.py

---
+++ utils/antisniper.py
@@ -0,0 +1,24 @@
+import requests
+
+from format.headers import setuprequests
+
+url, urlhyp, headers, headershyp = setuprequests()
+
+urlseraph = "https://antisniper.seraph.si/api/v4/"
+headersseraph = {
+    "run-api-key": "public",
+    "Content-Type": "application/json"
+}
+
+def seraphBlacklist(uuid):
+    r = requests.get(f"{urlseraph}/blacklist?uuid={uuid}", headers=headersseraph)
+    data = r.json()
+    if data["success"] == True:
+        return data["data"]
+    else:
+        raise Exception(f"{r.status_code}: {data}")
+
+def antisniper(uuid):
+    r = requests.get(f"{url}/antisniper?uuid={uuid}", headers=headers)
+    data = r.json()
+    return data
