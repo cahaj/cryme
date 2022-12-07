e741a72a981fa27091b7847e1b8ed90f4892983f
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-07 19:11:04 +0100

    Discord username lookup

--- utils/deleak.py
+++ utils/deleak.py
@@ -8,3 +8,12 @@ def lookup(discordID):
         if "discId" in i:
             if i["discId"] == discordID:
                 return i
+
+def lookupByUsername(username: str, discriminator: str):
+    with open("misc/DEUsers_9-28-2022.json", "r", encoding='utf-8') as f:
+        db = json.load(f)
+    
+    for i in db:
+        if "discriminator" in i:
+            if i["discriminator"] == discriminator and i["discUsername"] == username:
+                return i
