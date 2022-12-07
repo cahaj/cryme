07234eb69d369c6d3f85db643b31f5fbc4982687
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-07 19:14:53 +0100

    Update cmds.py

--- commands/cmds.py
+++ commands/cmds.py
@@ -2,9 +2,11 @@ from commands.slash.help import Help
 from commands.slash.wse import Wse
 from commands.slash.lastbans import LastBans
 from commands.slash.bancheck import BanCheck
+from commands.slash.blacklist import Blacklist
 
 def setup(bot):
     bot.add_cog(Help(bot))
     bot.add_cog(Wse(bot))
     bot.add_cog(LastBans(bot))
     bot.add_cog(BanCheck(bot))
+    bot.add_cog(Blacklist(bot))
