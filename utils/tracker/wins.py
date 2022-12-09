efc6a922a14c75d8d373013b645690cf94183002
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-09 17:06:08 +0100

    Duels wins tracking

---
+++ utils/tracker/wins.py
@@ -0,0 +1,33 @@
+import requests
+import time
+from datetime import datetime
+
+import json
+from jsondiff import diff
+
+from format.keys import setuprequests
+url, urlhyp, headers, headershyp = setuprequests()
+
+def duels(uuid: str, on: bool, speed: int):
+    while on == True:
+        r1 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
+        data1 = r1.json()
+        wins1 = data1["player"]["stats"]["Duels"]["wins"]
+        losses1 = data1["player"]["stats"]["Duels"]["losses"]
+
+        time.sleep(2)
+        r2 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
+        data2 = r2.json()
+        wins2 = data2["player"]["stats"]["Duels"]["wins"]
+        losses2 = data2["player"]["stats"]["Duels"]["losses"]
+
+        wins = diff(wins1, wins2)
+        losses = diff(losses1, losses2)
+        if bool(wins):
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            yield current_time, "WIN"
+        if bool(losses):
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            yield current_time, "LOSS"
