9425f0c8bac7bea11765a7f55a3a673441fd65cd
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-09 19:09:56 +0100

    Update wins.py

--- utils/tracker/wins.py
+++ utils/tracker/wins.py
@@ -8,26 +8,31 @@ from jsondiff import diff
 from format.keys import setuprequests
 url, urlhyp, headers, headershyp = setuprequests()
 
-def duels(uuid: str, on: bool, speed: int):
-    while on == True:
-        r1 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
-        data1 = r1.json()
-        wins1 = data1["player"]["stats"]["Duels"]["wins"]
-        losses1 = data1["player"]["stats"]["Duels"]["losses"]
+class ForceEnd(Exception): pass
+ 
+def duels(uuid: str, speed: int):
+    while True:
+        try:
+            r1 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
+            data1 = r1.json()
+            wins1 = data1["player"]["stats"]["Duels"]["wins"]
+            losses1 = data1["player"]["stats"]["Duels"]["losses"]
 
-        time.sleep(2)
-        r2 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
-        data2 = r2.json()
-        wins2 = data2["player"]["stats"]["Duels"]["wins"]
-        losses2 = data2["player"]["stats"]["Duels"]["losses"]
+            time.sleep(2)
+            r2 = requests.get(f"{urlhyp}/player?uuid={uuid}", headers = headershyp)
+            data2 = r2.json()
+            wins2 = data2["player"]["stats"]["Duels"]["wins"]
+            losses2 = data2["player"]["stats"]["Duels"]["losses"]
 
-        wins = diff(wins1, wins2)
-        losses = diff(losses1, losses2)
-        if bool(wins):
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            yield current_time, "WIN"
-        if bool(losses):
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            yield current_time, "LOSS"
+            wins = diff(wins1, wins2)
+            losses = diff(losses1, losses2)
+            if bool(wins):
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                yield current_time, "WIN"
+            if bool(losses):
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                yield current_time, "LOSS"
+        except ForceEnd:
+            break
